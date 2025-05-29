a5e0aa5d67a39fb3b60872b1196398a01a886b46
Oleh <dremliuhaoo.gmail.com>
2025-05-29 18:23:20 +0300

    Add password reset function

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.getMain, name="main"),
@@ -12,4 +13,8 @@ urlpatterns = [
     path('shop/', views.shop, name="shop"),
     path('product/<int:id>', views.getProduct, name="product"),
     path('addComent/<int:id>', views.addComent, name="addComent"),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/views.py
+++ main/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
 from .models import Product, Comment
 import datetime
+from django.contrib.auth.views import PasswordResetView
+from django.contrib.messages.views import SuccessMessageMixin
 
 userId = None
 
@@ -72,26 +75,32 @@ def logOut(request):
 @login_required
 def addProduct(request):
 
-    if request.method == "POST":
+    if request.user.is_staff:
 
-        name = request.POST['name']
-        description = request.POST['description']
-        try:
-            price = float(request.POST['price'])
-        except:
-            return render(request, 'main/addProductForm.html', {'error': 'Price must be number'})
-        try:
-            img = request.FILES['img']
-        except:
-            return render(request, 'main/addProductForm.html', {'error': 'File problem'})
-        
-        product = Product(name=name, description=description, price=price, img=img)
-        product.save()
+        if request.method == "POST":
+
+            name = request.POST['name']
+            description = request.POST['description']
+            try:
+                price = float(request.POST['price'])
+            except:
+                return render(request, 'main/addProductForm.html', {'error': 'Price must be number'})
+            try:
+                img = request.FILES['img']
+            except:
+                return render(request, 'main/addProductForm.html', {'error': 'File problem'})
+            
+            product = Product(name=name, description=description, price=price, img=img)
+            product.save()
+
+            return redirect('main')
+            
+
+        return render(request, 'main/addProductForm.html')
+    
+    else:
 
         return redirect('main')
-        
-
-    return render(request, 'main/addProductForm.html')
 
 
 def shop(request):
@@ -122,6 +131,23 @@ def addComent(request, id):
     coment.save()
 
     return redirect("product", id)
+
+
+
+
+
+
+
+
+class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
+    template_name = 'main/password_reset.html'
+    email_template_name = 'main/password_reset_email.html'
+    subject_template_name = 'main/password_reset_subject'
+    success_message = "We've emailed you instructions for setting your password, " \
+                      "if an account exists with the email you entered. You should receive them shortly." \
+                      " If you don't receive an email, " \
+                      "please make sure you've entered the address you registered with, and check your spam folder."
+    success_url = reverse_lazy('main')
 
 
 #Реалізувати Авторизацію та регістрацію
